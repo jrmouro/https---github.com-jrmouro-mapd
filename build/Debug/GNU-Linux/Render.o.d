b9bfc3a37e288ee47e3b274f9a29c91c3ad3ad59
jrmouro <jronaldomouro@google.com>
2023-07-28 23:59:25 -0300

    Render

---
+++ build/Debug/GNU-Linux/Render.o.d
@@ -0,0 +1,5 @@
+build/Debug/GNU-Linux/Render.o: Render.cpp Render.h Drawable.h
+
+Render.h:
+
+Drawable.h:
---
+++ build/Debug/GNU-Linux/main.o.d
@@ -0,0 +1,36 @@
+build/Debug/GNU-Linux/main.o: main.cpp InstanceExperiment.h InstanceMap.h \
+ SiteMap.h Site.h MapdException.h Identifiable.h Drawable.h Render.h \
+ EndPoint.h InstanceTask.h TaskMap.h Task.h Experiment.h AstarAlgorithm.h \
+ PathAlgorithm.h Path.h
+
+InstanceExperiment.h:
+
+InstanceMap.h:
+
+SiteMap.h:
+
+Site.h:
+
+MapdException.h:
+
+Identifiable.h:
+
+Drawable.h:
+
+Render.h:
+
+EndPoint.h:
+
+InstanceTask.h:
+
+TaskMap.h:
+
+Task.h:
+
+Experiment.h:
+
+AstarAlgorithm.h:
+
+PathAlgorithm.h:
+
+Path.h:
